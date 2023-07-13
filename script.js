8670c444addca4d191ae0cc13dd82ab68689515a
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-13 18:26:15 +0300

    list of all drivers fetch

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ fetch("http://localhost:3000/constructors")
   function driver(data){
     data.map(driver => displayDrivers(driver))
   }
+
+    //list of of all drivers
+
+    function displayDrivers(driver){
+
+       
+      let driverlist= document.querySelector("#driverlist")
+      let Driver1 = document.createElement("li");
+      Driver1.addEventListener("click",() =>(personalDetails(driver)));
+      Driver1.textContent = driver.Driver
+       driverlist.appendChild(Driver1)
+     }
+  
