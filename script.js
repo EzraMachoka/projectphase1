f72a5ca7142f2298eea497395eab7e62c77110bf
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-13 18:27:29 +0300

    function displaying each driver details

--- script.js
+++ script.js
@@ -33,3 +33,16 @@ fetch("http://localhost:3000/constructors")
        driverlist.appendChild(Driver1)
      }
   
+      //function for displaying details of each driver
+   function personalDetails(driver){
+    const gari  = document.getElementById("gari")
+    console.log(driver);
+    gari.innerHTML = ` <div class= "photo"><img src="${driver.Car_Photo}" alt="${driver.Car_Photo}" width = "250px" height = "200px"> </div>
+     <div class="detailss"><p><strong>The Team Principal is : </strong>${driver.Team_Principal}</p>
+     <p><strong>The Constructor is : </strong>${driver.Constructor}</p>
+     <p><strong>The Race Engineer is : </strong>${driver.Race_Engineer}</p> 
+     <p><strong>The Driver's favorite circuit is: </strong>${driver.Favorite_Circuit}</p>
+     <p><strong>The Car number is : </strong>${driver.Car_number}</p></div>`
+   
+gari.appendChild(gari)
+   }
