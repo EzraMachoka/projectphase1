60f0c709df6040349cd7be63965f62d7302e3606
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-13 18:23:56 +0300

    first event listener js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+        
+  document.querySelector('select').onclick = function() {
+      document.querySelector('#hello').style.color = this.value;
+  }
+
+ });
